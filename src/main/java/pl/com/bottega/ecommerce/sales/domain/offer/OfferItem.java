3314be9b2237e2e8d67e6a75037fe169a884c924
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-03-06 19:19:18 +0100

    Fixed hashCode, equals functions implementation. Changes caused by Discount extraction.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItem.java
@@ -76,10 +76,8 @@ public class OfferItem {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime
-				* result
-				+ ((discount.getDiscount() == null) ? 0 : discount
-						.getDiscount().hashCode());
+		result = prime * result
+				+ ((discount == null) ? 0 : discount.hashCode());
 		result = prime * result
 				+ ((productSnapshot == null) ? 0 : productSnapshot.hashCode());
 		result = prime * result + quantity;
@@ -97,10 +95,10 @@ public class OfferItem {
 		if (getClass() != obj.getClass())
 			return false;
 		OfferItem other = (OfferItem) obj;
-		if (discount.getDiscount() == null) {
-			if (other.discount.getDiscount() != null)
+		if (discount == null) {
+			if (other.discount != null)
 				return false;
-		} else if (!discount.getDiscount().equals(other.discount.getDiscount()))
+		} else if (!discount.equals(other.discount))
 			return false;
 		if (productSnapshot == null) {
 			if (other.productSnapshot != null)
